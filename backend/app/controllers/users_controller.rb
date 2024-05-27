317f15342f6dbcab640c4740a8aace41c17be9d5
yoshihiro <yoshi.maruyama02@gmail.com>
2024-05-27 15:02:20 +0800

    Implemented user create update feat

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,6 +1,37 @@
 class UsersController < ApplicationController
 
-  def edit
+  def show
+  end
 
+  def create
+    user.assign_attributes(user_params)
+    if user.save
+      flash[:success] = "Record successfully created"
+      redirect_to user_path
+    else
+      flash.now[:error] = "Failed to create Record"
+      render :show
+    end
+  end
+
+  def update
+    user.assign_attributes(user_params)
+    if user.save
+      flash[:success] = "Record successfully updated"
+      redirect_to user_path
+    else
+      flash.now[:error] = "Failed to update Record"
+      render :show
+    end
+  end
+
+  def user
+    @user ||= User.first || User.new
+  end
+  helper_method :user
+
+
+  def user_params
+    params.require(:user).permit(:name, :email, :postal_code, :address)
   end
 end
