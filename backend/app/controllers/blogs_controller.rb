516a29f6d1a23be27cf4fadc686f7ce6631d86da
yoshi-maruyama <yoshi.maruyama02@gmail.com>
2024-06-30 14:03:59 +0900

    Implemented blog index dynamic

--- backend/app/controllers/blogs_controller.rb
+++ backend/app/controllers/blogs_controller.rb
@@ -1,4 +1,15 @@
 class BlogsController < ApplicationController
   def index
   end
+
+  private
+
+  def user
+    @user ||= User.first
+  end
+
+  def blogs
+    @blogs ||= user.blogs
+  end
+  helper_method :blogs
 end
