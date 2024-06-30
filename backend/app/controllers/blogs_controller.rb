d3270fcf911caa8f2e73007bc08567f00e15988e
yoshi-maruyama <yoshi.maruyama02@gmail.com>
2024-06-30 14:23:39 +0900

    Implemented create blog by skip notion key

--- backend/app/controllers/blogs_controller.rb
+++ backend/app/controllers/blogs_controller.rb
@@ -5,6 +5,17 @@ class BlogsController < ApplicationController
   def new
   end
 
+  def create
+    blog.assign_attributes(blog_params)
+    if blog.save
+      flash[:success] = "Record successfully created"
+      redirect_to blogs_path
+    else
+      flash.now[:error] = "Failed to create Record"
+      render :new
+    end
+  end
+
   private
 
   def user
@@ -20,4 +31,8 @@ class BlogsController < ApplicationController
     @blog ||= blogs.build
   end
   helper_method :blog
+
+  def blog_params
+    params.require(:blog).permit(:title, :summary)
+  end
 end
